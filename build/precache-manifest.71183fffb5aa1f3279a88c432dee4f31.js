94774d159831499b3234823de0b1ef9c26b389cb
yagnik1106 <Yagnikpatel2028@gmail.com>
2022-10-04 14:57:11 -0400

    React project code added

---
+++ build/precache-manifest.71183fffb5aa1f3279a88c432dee4f31.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "15ad00e2e16239f07043",
+    "url": "/Yagnik0611/COMP3104/static/css/main.41d48ccc.chunk.css"
+  },
+  {
+    "revision": "15ad00e2e16239f07043",
+    "url": "/Yagnik0611/COMP3104/static/js/main.15ad00e2.chunk.js"
+  },
+  {
+    "revision": "a1873dcaf519ed528a1b",
+    "url": "/Yagnik0611/COMP3104/static/js/runtime~main.a1873dca.js"
+  },
+  {
+    "revision": "8ebec04f6c698faf83f8",
+    "url": "/Yagnik0611/COMP3104/static/js/2.8ebec04f.chunk.js"
+  },
+  {
+    "revision": "b78866565b40b13dda29b88bbb44c749",
+    "url": "/Yagnik0611/COMP3104/index.html"
+  }
+];
